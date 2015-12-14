2cebbf0280e9a3f64afe53497eca505a229c074b
Maria Hejlek <mh@getaweb.at>
2015-12-14 17:18:28 +0100

    changed migrations to south migrations, added new django migrations

--- attachments/migrations/0001_initial.py
+++ attachments/migrations/0001_initial.py
@@ -1,95 +1,31 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+import attachments.models
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Attachment'
-        db.create_table(u'attachments_attachment', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.User'])),
-            ('file', self.gf('django.db.models.fields.files.FileField')(max_length=255)),
-            ('org_filename', self.gf('django.db.models.fields.TextField')()),
-            ('suffix', self.gf('django.db.models.fields.CharField')(default='', max_length=8, blank=True)),
-            ('is_img', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('num_downloads', self.gf('django.db.models.fields.IntegerField')(default=0)),
-            ('description', self.gf('django.db.models.fields.TextField')(default='', blank=True)),
-            ('activated', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('date_uploaded', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-        ))
-        db.send_create_signal(u'attachments', ['Attachment'])
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
 
-
-    def backwards(self, orm):
-        # Deleting model 'Attachment'
-        db.delete_table(u'attachments_attachment')
-
-
-    models = {
-        u'attachments.attachment': {
-            'Meta': {'object_name': 'Attachment'},
-            'activated': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'date_uploaded': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'description': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
-            'file': ('django.db.models.fields.files.FileField', [], {'max_length': '255'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_img': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'num_downloads': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
-            'org_filename': ('django.db.models.fields.TextField', [], {}),
-            'suffix': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '8', 'blank': 'True'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.User']"})
-        },
-        u'auth.group': {
-            'Meta': {'object_name': 'Group'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
-        },
-        u'auth.permission': {
-            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
-            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        },
-        u'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        u'core.user': {
-            'Meta': {'object_name': 'User'},
-            'course_provider_state': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0', 'blank': 'True'}),
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'department_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_course_contact': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_course_provider': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_course_seeker': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'job_title': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'profile_completed': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'skip_profile_completed_hint': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'suggested_provider': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
-            'telephone': ('django.db.models.fields.CharField', [], {'max_length': '150', 'null': 'True', 'blank': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
-            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
-        }
-    }
-
-    complete_apps = ['attachments']
+    operations = [
+        migrations.CreateModel(
+            name='Attachment',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('file', models.FileField(max_length=255, upload_to=attachments.models.upload_attachment_file_path)),
+                ('org_filename', models.TextField()),
+                ('suffix', models.CharField(default=b'', max_length=8, blank=True)),
+                ('is_img', models.BooleanField(default=False)),
+                ('num_downloads', models.IntegerField(default=0)),
+                ('description', models.TextField(default=b'', blank=True)),
+                ('activated', models.BooleanField(default=False)),
+                ('date_uploaded', models.DateTimeField(auto_now_add=True)),
+                ('user', models.ForeignKey(verbose_name='Attachment', to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
